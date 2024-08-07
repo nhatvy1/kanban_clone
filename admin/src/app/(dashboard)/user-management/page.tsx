2ce9f8133659a061f307bca2611c142f65f60c68
nhatvy1 <vyhuynhak123@gmail.com>
2024-08-07 15:03:36 +0700

    vendor: install tanstack table

--- admin/src/app/(dashboard)/user-management/page.tsx
+++ admin/src/app/(dashboard)/user-management/page.tsx
@@ -1,6 +1,19 @@
-const UserManagementPage = ()=> {
+import { columns, Payment } from "@/components/pages/user-management/columns"
+import { UserTable } from "@/components/pages/user-management/UserTable"
+
+const UserManagementPage = () => {
+  const data: Payment[] = [
+    {
+      id: 1,
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    }
+  ]
   return (
-    <div>User Management Page</div>
+    <div>
+      <UserTable data={data} columns={columns}/>
+    </div>
   )
 }
 
