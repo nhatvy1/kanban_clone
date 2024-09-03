27e0c5b15ebfc4c679c783c706042287b3c2569e
nhatvy1 <vyhuynhak123@gmail.com>
2024-09-03 17:41:30 +0700

    feat(dashboard): authorization route

--- dashboard/src/utils/constant.ts
+++ dashboard/src/utils/constant.ts
@@ -2,3 +2,18 @@ export enum HttpStatusEnum {
   AUTHORIZATION = 401,
   FORBIDDEN = 403
 }
+
+export enum Modules {
+  USER = 'user',
+  TEAM = 'team',
+  PROJECT = 'project',
+  ROLE = 'role'
+}
+
+export enum Action {
+  CREATE = 'create',
+  READ = 'read',
+  UPDATE = 'update',
+  DELETE = 'delete',
+  MANAGE = 'manage'
+}
